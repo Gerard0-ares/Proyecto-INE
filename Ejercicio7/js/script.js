973749866be4f5d22f0b6ec95bcf99179928ace7
Gerard0-ares <132033858+Gerard0-ares@users.noreply.github.com>
2023-04-29 00:15:35 -0500

    Delete Ejercicio7 directory

--- Ejercicio7/js/script.js
+++
@@ -1,9 +0,0 @@
-// Clase de como utilizar for
-
-for(let a=0; a <= 10; a++){
-    document.write(`a vale: ${a} <br> `);
-}
-
-for(let a=10; a >= 0; a--){
-    document.write(`a vale: ${a} <br> `);
-} 
